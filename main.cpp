cffa7b2e3661ecf72f177b4a0af5ccfd8f855000
yanghua <codehunter@msn.com>
2016-07-19 17:34:20 +0800

    Init

--- main.cpp
+++ main.cpp
@@ -1,8 +1,30 @@
+#include <boost/iostreams/filtering_streambuf.hpp>
+#include <boost/iostreams/copy.hpp>
+#include <boost/iostreams/filter/zlib.hpp>
+#include <boost/iostreams/device/file_descriptor.hpp>
+#include <boost/iostreams/device/file.hpp>
+
 #include <iostream>
+#include <sstream>
 
 using namespace std;
+using namespace boost::iostreams;
 
 int main() {
-    cout << "Hello, World!" << endl;
+    ofstream ofile("hello.z", ios_base::out | ios_base::binary);
+    filtering_streambuf<output> out;
+    out.push(zlib_compressor());
+    out.push(ofile);
+
+    std::istringstream is("11111111111111111111111111111111111111111111111111111111111111111111111111111111111111");
+    boost::iostreams::copy(is, out);
+
+    ifstream file("hello.z", ios_base::in | ios_base::binary);
+    filtering_streambuf<input> in;
+    in.push(zlib_decompressor());
+    in.push(file);
+    boost::iostreams::copy(in, cout);
+    system("pause");
     return 0;
 }
+
