dbed18c55928d865e4dc9b1476a6b1a1a6448802
yanghua <codehunter@msn.com>
2016-07-20 21:24:57 +0800

    hh

--- main.cpp
+++ main.cpp
@@ -1,29 +1,11 @@
-#include <boost/iostreams/filtering_streambuf.hpp>
-#include <boost/iostreams/copy.hpp>
-#include <boost/iostreams/filter/zlib.hpp>
-#include <boost/iostreams/device/file_descriptor.hpp>
-#include <boost/iostreams/device/file.hpp>
-
 #include <iostream>
 #include <sstream>
+#include <ThostFtdcMdApi.h>
+#include <ThostFtdcTraderApi.h>
 
-using namespace std;
-using namespace boost::iostreams;
 
 int main() {
-    ofstream ofile("hello.z", ios_base::out | ios_base::binary);
-    filtering_streambuf<output> out;
-    out.push(zlib_compressor());
-    out.push(ofile);
 
-    std::istringstream is("11111111111111111111111111111111111111111111111111111111111111111111111111111111111111");
-    boost::iostreams::copy(is, out);
-
-    ifstream file("hello.z", ios_base::in | ios_base::binary);
-    filtering_streambuf<input> in;
-    in.push(zlib_decompressor());
-    in.push(file);
-    boost::iostreams::copy(in, cout);
     system("pause");
     return 0;
 }
